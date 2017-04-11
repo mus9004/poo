b668c83afbe8db4be811124ae076a451c84f78f7
mus9004 <mus9004@yahoo.es>
2017-04-10 22:35:45 -0600

    update

--- Cuenta.php
+++ Cuenta.php
@@ -5,6 +5,12 @@
 	<title>Tipo Usuario</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	 <link rel="icon" type="image/icon" href="images/netbit.ico" />
+	 <link rel="stylesheet" type="text/css" href="css/signin.css">
+
+	<script type="text/javascript" src="js/jquery.min.js" ></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="js/bootstrap.min.js" ></script>
+	<script type="text/javascript" src="js/jquery.min.js" ></script>
 
 </head>
 <body>
@@ -35,16 +41,18 @@
 		<tr>
 			<td>
 				<div class="col-md-1  col-lg-1"></div> 
-				<div class="col-md-8 col-lg-8">Correo@yahoo.com</div> 
-				<div lass="col-md-3 col-lg-3"><span style="color: #4286f4">cambiar</span>
-				</div>
+				<div class="col-md-7 col-lg-7">Correo@yahoo.com</div> 
+				<div class="col-md-1 col-lg-1"><span style="color: #4286f4"><label id="Ccorreo"><a>cambiar</a></label> </span></div>
+				<div class="col-md-3 col-lg-3"><input type="email" name="txt-email" id="txt-email" class="form-control"  style="size: 20%"></div>
 			</td>
+			
 		</tr>
 		<tr>
 			<td>
 				<div class="col-md-1  col-lg-1"></div> 
-				<div class="col-md-8 col-lg-8">Contraseña ********</div> 
-				<div lass="col-md-9 col-lg-3 col-lg-3 col-lg-3"><span style="color: #4286f4">cambiar</span></div>
+				<div class="col-md-7 col-lg-7">Contraseña ********</div> 
+				<div class="col-md-1 col-lg-1  "><span style="color: #4286f4"><label id="contrasena"><a>cambiar</a></label> </span></div>
+				<div class="col-md-3 col-lg-3"><input type="password" name="txt-password" id="txt-password" class="form-control"  style="size: 20%"></div>
 			</td>
 		</tr>
 		
@@ -54,14 +62,26 @@
 		<tr>
 			<td>
 				<div class="col-md-1  col-lg-1"></div> 
-				<div class="col-md-8  col-lg-8"><h4>Ultra HD</h4> </div> 
-				<div lass="col-md-9  col-lg-3"><span style="color: #4286f4">cambiar</span></div>
+				<div class="col-md-7  col-lg-7"><h4>Ultra HD</h4> </div> 
+				<div lass="col-md-1  col-lg-1"><span style="color: #4286f4"><label id="plan"><a>cambiar</a></label> </span></div>
+				<div class="col-md-3 col-lg-3">
+					<label id="hd"><input type="radio" name="txt-plan" id="txt-hd"   style="size: 20%">HD</label>
+					<label id="dvd"><input type="radio" name="txt-plan" id="txt-dvd"   style="size: 20%">DVD</label>
+					<label id="ultra"><input type="radio" name="txt-plan" id="txt-ultra"  style="size: 20%">ultra HD</label>
+				</div>
 			</td>
 		</tr>
 		<tr>
 			<td>
-				<div class="col-md-9 col-md-9 col-lg-9 col-lg-9"></div> 
-				<div lass="col-md-9 col-lg-3 col-lg-3 col-lg-3"><span style="color: #4286f4">Agregar plan</span></div>
+				<div class="col-md-1  col-lg-1"></div> 
+				<div class="col-md-7  col-lg-7"><h4></h4>. </div> 
+				<div lass="col-md-1  col-lg-1"><span style="color: #4286f4"><label  id="Agregar"><a>cambiar</a> </label> </span></div>
+				<div class="col-md-3 col-lg-3">
+					<label id="hd1"><input type="radio" name="txt-plan" id="txt-hd1"   style="size: 20%">HD</label>
+					<label id="dvd1"><input type="radio" name="txt-plan" id="txt-dvd1"   style="size: 20%">DVD</label>
+					<label id="ultra1"><input type="radio" name="txt-plan" id="txt-ultra1"  style="size: 20%">ultra HD</label>
+				</div>
+				
 			</td>
 		</tr>
 		<tr style="border-bottom: 3px solid #ddd;">
@@ -70,11 +90,8 @@
 		<tr>
 			<td>
 				<div class="col-md-1  col-lg-1"></div> 
-				<div class="col-md-8 col-lg-8">
-					Control Parental<br>
-					Activar dispositivos<br>
-					Cerrar secion de dispositivos<br>
-				</div> 
+				<div class="col-md-7 col-lg-7">	<a id="control"> Control Parental</a></div> 
+				<div class="col-md-1 col-lg-1">	<a id="activar"> Activar</a></div> 
 				
 			</td>
 		</tr>
@@ -83,6 +100,57 @@
 		
 		
 	</div>
+
+	<div class="cuadro1" style="margin-top: 200px;">
+          
+        <span style="margin-left: 45px;">¿Tienes preguntas? Llama al<a class="condiciones" href=""> +(504) 9206-0911 (HND)</a></span>
+        <ul>
+          <li><a href="">Terminos de las tarjetas de regalo</a></li>
+          <li><a href="">Terminos de uso</a></li>
+          <li><a href="">Declaracion de privacidad.</a></li>
+        </ul><br>
+        <select class="form-control idiomas">
+          <option>Español</option>
+          <option>English</option>
+        </select>
+              
+    </div>
+    <script type="text/javascript">
+    	$(document).ready(function(){
+			$("#txt-email").hide();
+			$("#txt-password").hide();
+			$("#hd").hide();
+			$("#dvd").hide();
+			$("#ultra").hide();
+			$("#hd1").hide();
+			$("#dvd1").hide();
+			$("#ultra1").hide();
+			$("#activar").hide();
+			
+		});
+
+    	$("#Ccorreo").click(function(){
+
+    		$("#txt-email").show("swing");
+    	});
+    	$("#contrasena").click(function(){
+    		$("#txt-password").show("swing");
+    	});
+    	$("#plan").click(function(){
+    		$("#hd").show("swing");
+			$("#dvd").show("swing");
+			$("#ultra").show("swing");
+    	});
+    	$("#Agregar").click(function(){
+    		$("#hd1").show("swing");
+			$("#dvd1").show("swing");
+			$("#ultra1").show("swing");
+    	});
+    	$("#control").click(function(){
+    		$("#activar").show("swing");
+    	});
+    </script>
+    
 
 </body>
 </html>
