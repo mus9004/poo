92c2ce445eaf846cc6f1008b97bdbde70342a645
LeviCanales <milevi271@gmail.com>
2017-04-11 21:01:48 -0600

    gif nuevo

--- ajax/procesar-velocidad.php
+++ ajax/procesar-velocidad.php
@@ -10,4 +10,7 @@ echo $time.' segundos<br>';
 $size = /*$size / 102400*/1024;
 $speed = round(($size / $time),2);
 echo "Tu velocidad es: $speed Kb/s";
+if($speed>2048){
+	echo "Pucha man, descargame una peli xD";
+}
 ?>
